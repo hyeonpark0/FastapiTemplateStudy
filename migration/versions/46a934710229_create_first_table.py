0e360d8931603cc8d688f5ae82ee36c95923383e
EC2 Default User <ec2-user@ip-172-31-90-132.ec2.internal>
2023-04-12 07:46:35 +0000

    add versions

---
+++ migration/versions/46a934710229_create_first_table.py
@@ -0,0 +1,24 @@
+"""create first table
+
+Revision ID: 46a934710229
+Revises: 
+Create Date: 2023-04-12 07:42:32.426355
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '46a934710229'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
